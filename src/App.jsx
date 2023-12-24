6ee16730924c362bc24f8631999c0396a64179e5
WiseyXD <aryan.s.nag@gmail.com>
2023-12-24 11:25:35 +0530

    Router Setup done

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
 
 function App() {
     const [count, setCount] = useState(0);
 
-    return <>
-        <h1>Hello</h1>
-    </>;
+    return (
+        <>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/login" element={<Login />} />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
